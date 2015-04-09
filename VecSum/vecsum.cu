8060b0fdda19501dde858650ef1c238a854b947f
alsuga <ing.alsuga@gmail.com>
2015-04-09 13:39:50 -0500

    working

--- VecSum/vecsum.cu
+++ VecSum/vecsum.cu
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-const int BLOCK_SIZE = 64;
+#define BLOCK_SIZE 1024
+
+int sz(int a, int b) {
+  return (a + b -1) / b;
+}
 
 //vector initialization
 void init(int *A,int n, int d){
@@ -20,7 +24,7 @@ void vecSum(int *h_A, int *h_B, int n){
 
 //Parallel kernel
 __global__ void vecSumP (int *A, int *out, int n){
-  __shared__ float tmp[BLOCK_SIZE];
+  __shared__ int tmp[BLOCK_SIZE];
   size_t t = threadIdx.x;
   size_t i = blockDim.x * blockIdx.x + t;
   if(i < n)
@@ -28,10 +32,10 @@ __global__ void vecSumP (int *A, int *out, int n){
   else
     tmp[t] = 0;
   __syncthreads();
-  for(size_t st = blockDim.x; st > 1; st >>= 1){
-    __syncthreads();
+  for(size_t st = blockDim.x/2; st > 0; st >>= 1){
     if(t < st)
       tmp[t] += tmp[t + st];
+    __syncthreads();
   }
   __syncthreads();
   if (t == 0) out[blockIdx.x] = tmp[0];
@@ -39,9 +43,12 @@ __global__ void vecSumP (int *A, int *out, int n){
 
 int main(){
   int n; cin>>n;
+  cudaError_t error = cudaSuccess;
   int size = n * sizeof(int);
   int *h_A = (int *)malloc(size);
-  int *h_B = (int *)malloc((size + BLOCK_SIZE - 1)/BLOCK_SIZE);
+  int *h_tmp = (int *)malloc(size);
+  int *h_B = (int *)malloc(sizeof(int));
+  //int *h_C = (int *)malloc(sz(size,BLOCK_SIZE));
   int *h_C = (int *)malloc(size);
   int *d_A, *d_B;
   init(h_A, n, 1);
@@ -49,37 +56,67 @@ int main(){
   //double a, b;
   clock_t t = clock();
 
-  //Secuencial
+  //Secuencial ***************
   vecSum(h_A, h_B, n);
   t = clock() - t;
   //a = ((float)t)/CLOCKS_PER_SEC;
-  cout<<*h_B<<endl;
+  cout<<"secuential: "<<h_B[0]<<endl;
 
-  //paralelo
+  //paralelo*****************
   t = clock();
-
+  cout<<"size: "<<size<<endl;
+  error = cudaMalloc(&d_A, size);
+  if(error != cudaSuccess){
+    printf("Error reservando memoria para d_A");
+    exit(0);
+  }
+  error = cudaMalloc(&d_B, sz(n, BLOCK_SIZE) * sizeof(int));
+  if(error != cudaSuccess){
+    printf("Error reservando memoria para d_B");
+    exit(0);
+  }
+  error = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+  if(error != cudaSuccess){
+    printf("Error copiando h_A en d_A");
+    exit(0);
+  }
+  while(n > 1) {
+    vecSumP<<< sz(n, BLOCK_SIZE), BLOCK_SIZE>>> (d_A, d_B, n);
+    error = cudaGetLastError();
+    if ( cudaSuccess != error ){
+      cout<<"Error en el kernel!"<<endl;
+      cout<<cudaGetErrorString( error )<<endl;
+      exit(0);
+    }
+    n = sz(n, BLOCK_SIZE);
+    error = cudaMemcpy(d_A, d_B, n * sizeof(int), cudaMemcpyDeviceToDevice);
+    if(error != cudaSuccess){
+      printf("Error copiando d_A en d_B");
+      cout<<cudaGetErrorString( error )<<endl;
+      exit(0);
+    }
+    cudaDeviceSynchronize();
+  }
   //Allocate memory for device
-  cudaMalloc(&d_A, size);
-  cudaMalloc(&d_B, sizeof(int));
+ // cudaMalloc(&d_A, size);
+ // cudaMalloc(&d_B, sizeof(int));
 
   //Copy Data from host to device
-  cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+  //cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
   //Blocks and Grids
 
   //Launch Kernel
-  vecSumP<<< (n + BLOCK_SIZE - 1)/BLOCK_SIZE, BLOCK_SIZE>>> (d_A, n);
+  //vecSumP<<< (n + BLOCK_SIZE - 1)/BLOCK_SIZE, BLOCK_SIZE>>> (d_A, d_B,n);
   cudaDeviceSynchronize();
 
   //Copy from device, free device memory
-  cudaMemcpy (h_C, d_A, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy (h_C, d_B, sizeof(int), cudaMemcpyDeviceToHost);
 
 
   //matMultP(A,B,D,size);
   t = clock() - t;
-  cout<<"parallel"<<endl;
+  cout<<"parallel: "<<h_C[0]<<endl;
   //b = ((float)t)/CLOCKS_PER_SEC;
-  for(int i = 0; i < n; i++)
-    cout<<h_C[i]<<endl;
   //cout<<(a / b)<<endl;
   //printmat(C,n);
   //printmat(D,n);
@@ -91,6 +128,6 @@ int main(){
   free(h_B);
   free(h_C);
   cudaFree(d_A);
-  //cudaFree(d_B);
+  cudaFree(d_B);
   return 0;
 }
