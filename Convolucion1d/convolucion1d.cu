e0a13b132218b6c27d702803fa95d01d1a42eaa5
alsuga <ing.alsuga@gmail.com>
2015-04-13 20:45:09 -0500

    Convolution working

--- Convolucion1d/convolucion1d.cu
+++ Convolucion1d/convolucion1d.cu
@@ -3,8 +3,10 @@ using namespace std;
 #define __ ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define endl '\n'
 #define KERNEL_SIZE 3
-#define BLOCK_SIZE 1024
-#define gpu_error(ans) { gpuAssert((ans), __LINE__); }
+#define BLOCK_SIZE 4
+#define gpu_error(ans) { gpu_assert((ans), __LINE__); }
+
+__constant__ int d_cachekernel[KERNEL_SIZE];
 
 inline void gpu_assert(cudaError_t code, int line){
   if (code != cudaSuccess)
@@ -34,26 +36,55 @@ void convolSec(int *vector, int *kernel, int *out, int n) {
   }
 }
 
-__global__ void convolPar(int *d_vec, int *d_out, int n) {
+__global__ void convolParB(int *d_vec, int *d_kernel, int *d_out, int n) {
+  int i  = blockIdx.x * blockDim.x + threadIdx.x;
+
+  int tmp = i - (KERNEL_SIZE/2), sum = 0;
+  for(int j = 0; j < KERNEL_SIZE; j++) {
+    if(tmp + j >= 0 and tmp + j < n)
+      sum += d_vec[tmp + j] * d_kernel[j];
+  }
+  //__syncthreads();
+  if(i < n)
+    d_out[i] = sum;
+}
+
+__global__ void convolParB(int *d_vec, int *d_out, int n) {
+  int i  = blockIdx.x * blockDim.x + threadIdx.x;
+
+  int tmp = i - (KERNEL_SIZE/2), sum = 0;
+  for(int j = 0; j < KERNEL_SIZE; j++) {
+    if(tmp + j >= 0 and tmp + j < n)
+      sum += d_vec[tmp + j] * d_cachekernel[j];
+  }
+  //__syncthreads();
+  if(i < n)
+    d_out[i] = sum;
+}
+
+__global__ void convolParT(int *d_vec, int *d_out, int n) {
   __shared__ int tile[BLOCK_SIZE + KERNEL_SIZE - 1];
 
-  size_t i     = blockIdx.x * blockDim.x + threadIdx.x;
-  size_t left  = (blockIdx.x - 1 * blockDim.x) + threadIdx.x;
-  size_t rigth = (blockIdx.x + 1 * blockDim.x) + threadIdx.x;
-  int tmp = 0, n = KERNEL_SIZE/2;
+  size_t i  = blockIdx.x * blockDim.x + threadIdx.x;
+  int left  = ((blockIdx.x - 1) * blockDim.x) + threadIdx.x;
+  int rigth = ((blockIdx.x + 1) * blockDim.x) + threadIdx.x;
+  int wn = KERNEL_SIZE/2;
 
-  if(threadIdx.x >= blockDim.x - n)
-    tile[threadIdx.x - blockDim.x + n] = (left < 0)? 0 : d_vec[left];
+  if(threadIdx.x >= blockDim.x - wn)
+    tile[threadIdx.x - (blockDim.x - wn)] = (left < 0)? 0 : d_vec[left];
+    //tile[threadIdx.x - blockDim.x + wn] = (left < 0)? 0 : d_vec[left];
 
-  tile[i] = d_vec[i];
+  tile[wn + threadIdx.x] = (i < n)? d_vec[i] : 0;
 
-  if(threadIdx.x < n)
-    tile[threadIdx.x + blockDim.x + n] = (rigth >= n)? 0 : d_vec[rigth];
+  if(threadIdx.x <= wn)
+    tile[threadIdx.x + blockDim.x + wn] = (rigth >= n)? 0 : d_vec[rigth];
 
   __syncthreads();
 
-  for(int j = 0; j < KERNEL_SIZE: j++) {
-    tmp += tile[threadIdx.x + j] * d_kernel[j];
+  int tmp = 0;
+  for(int j = 0; j < KERNEL_SIZE; j++) {
+    tmp += tile[threadIdx.x + j] * d_cachekernel[j];
+    //d_out[i]= tile[i+1];
   }
   d_out[i] = tmp;
 }
@@ -75,22 +106,20 @@ int main(){__
   print(h_out, n);
 
   // <--------- Paralelo --------->
-  cudaError_t error = cudaSuccess;
-  int *d_vec, *d_out; //, d_kernel;
-  int sz = sizeof(int);
-  //gpu_error(cudaMalloc(&d_kernel, sz * KERNEL_SIZE));
-  //gpu_error(cudaMemcpy(d_kernel, h_kernel, KERNEL_SIZE, cudaMemcpyHostToDevice));
-  __constant__ int d_kernel[KERNEL_SIZE];
-  gpu_error(cudaMemcpyToSymbol(d_kernel, h_kernel, KERNEL_SIZE));
-  gpu_error(cudaMalloc(&d_vec, sz * n));
-  gpu_error(cudaMemcpy(d_vec, h_vec, n, cudaMemcpyHostToDevice));
-  gpu_error(cudaMalloc(&d_out, sz * n));
+  int *d_vec, *d_out, *d_kernel;
+  int sz = sizeof(int) * n;
+  //gpu_error(cudaMalloc(&d_kernel, sizeof(int) * KERNEL_SIZE));
+  //gpu_error(cudaMemcpy(d_kernel, h_kernel, sizeof(int)*KERNEL_SIZE, cudaMemcpyHostToDevice));
+  gpu_error(cudaMemcpyToSymbol(d_cachekernel, h_kernel, sizeof(int) * KERNEL_SIZE));
+  gpu_error(cudaMalloc(&d_vec, sz));
+  gpu_error(cudaMemcpy(d_vec, h_vec, sz, cudaMemcpyHostToDevice));
+  gpu_error(cudaMalloc(&d_out, sz ));
 
-  convolPar<< size(n, BLOCK_SIZE), BLOCK_SIZE >>(d_vec, d_out, n);
+  //convolParB<<< size(n, BLOCK_SIZE), BLOCK_SIZE >>> (d_vec, d_kernel, d_out, n);
+  convolParT<<< size(n, BLOCK_SIZE), BLOCK_SIZE >>> (d_vec, d_out, n);
   gpu_error(cudaGetLastError());
   cudaDeviceSynchronize();
-  gpu_error(cudaMemcpy(out_d, d_out, n, cudaMemcpyDeviceToHost));
-
+  gpu_error(cudaMemcpy(out_d, d_out, sz, cudaMemcpyDeviceToHost));
   print(out_d, n);
   return 0;
 }
